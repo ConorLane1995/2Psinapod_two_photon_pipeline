b37ac3e6189ecd4666bb23e3eadd31e4963e5600
Veronica Tarka <veronica@dhcp58-162.mni.mcgill.ca>
2021-11-09 17:38:13 -0500

    Added config file support

---
+++ scripts/make_config.py
@@ -0,0 +1,57 @@
+'''
+Script to create the config file to be used by all analysis scripts.
+Should be the first thing you run.
+Started working: Nov. 9 2021
+Author: Veronica Tarka, veronica.tarka@mail.mcgill.ca
+WON'T ACCEPT ~ TO MEAN HOME
+'''
+
+# IMPORT STATEMENTS
+import os.path as osp
+import argparse
+import sys
+
+def main():
+    ###### Get the path to where 2Psinapod is installed #######
+    # Option to have the path passed in when the script is run
+    parser = argparse.ArgumentParser(description='Get the directory for the 2P analysis scripts')
+    parser.add_argument('-d','--proj_dir', required=False, help='Path to the 2Psinapod folder')
+    args = parser.parse_args()
+
+    if args.proj_dir is not None:
+        if osp.isdir(args.proj_dir):
+            wd = args.proj_dir
+        else:
+            print("Directory provided is invalid")
+            sys.exit()
+    else:
+        wd_input = input("Enter the path to the 2Psinapod folder: ")
+
+        if osp.isdir(wd_input):
+            wd = wd_input
+        else:
+            print("Directory provided is invalid")
+            sys.exit()
+
+
+    # Check if the config file already exists, and prompt to overwrite it
+    full_config_path = wd + "/config.json"
+    if osp.exists(full_config_path):
+        overwrite_yn = input("A config file already exists, do you want to overwrite it? ")
+        overwrite_yn = overwrite_yn.lower()
+        if overwrite_yn == "no":
+            print("Ok, exiting script.")
+            sys.exit()
+        elif overwrite_yn == "yes":
+            print("Ok, continuing on...")
+        else:
+            print("Input not understood. Expecting yes or no.")
+
+    config_dict = {}
+    config_dict['WorkingDir'] = wd
+
+    # First make sure we don't already have a config file
+    #if osp.exists("")
+
+if __name__=='__main__':
+    main()
