f1abc9735b72e39e6219d853682a4dc20f7ef740
vtarka00 <vmtarka@gmail.com>
2022-02-19 13:03:28 -0500

    Added tuning

--- src/find_active_cells.py
+++ src/find_active_cells.py
@@ -14,44 +14,63 @@ from scipy.stats import zscore
 
 BASE_PATH = "D:/vid127_pseudorandom_stim/"
 traces_file = "cell_traces_with_stims.pkl"
-output_file = "traces_with_activity_boolean.pkl"
-EPOCH_START = -500
-EPOCH_END = 2700
-fs = 10
+output_file = "traces_with_activity_boolean_2.pkl"
+EPOCH_START_IN_MS = -500 # time before trial onset included in the epoch
+EPOCH_END_IN_MS = 2700 # time after trial onset included in the epoch
+FRAMERATE = 10
+
+# def check_cell_STD(cell_trace,n_baseline_frames,STD_threshold):
+#     # cell_trace is going to be all the trials of this one cell
+#     # {freq: intensity: repetition: trace = [x,x,x,x,...]}}}
+
+#     # we'll say a cell is active if it reaches beyond the peak threshold on >1 trial on >0 frequencies
+#     reached_threshold_counter = 0 # need this to be >1 to return true
+
+#     # go through each frequency
+#     for freq in cell_trace:
+
+#         # go through each intensity it was played at:
+#         for intensity in cell_trace[freq]:
+
+#             # go through ecah repetition
+#             for repetition in cell_trace[freq][intensity]:
+
+#                 this_trial = cell_trace[freq][intensity][repetition]
+
+#                 # get the baseline for this trial
+#                 baseline = this_trial[0:n_baseline_frames-1]
+#                 response = this_trial[n_baseline_frames:]
+
+#                 # get our threshold
+#                 peak_threshold = np.mean(baseline) + STD_threshold*np.std(baseline)
+
+#                 # get the peak response
+#                 peak_response = np.amax(response)
+
+#                 # if our peak response was above our threshold, increase the counter
+#                 if peak_response >= peak_threshold:
+#                     reached_threshold_counter += 1
+
+#     if reached_threshold_counter > 1:
+#         return True
+#     else:
+#         return False
 
 def check_cell_STD(cell_trace,n_baseline_frames,STD_threshold):
-    # cell_trace is going to be all the trials of this one cell
-    # {freq: intensity: repetition: trace = [x,x,x,x,...]}}}
+    this_cell_trace = get_avg_trace(cell_trace)
 
-    # we'll say a cell is active if it reaches beyond the peak threshold on >1 trial on >0 frequencies
-    reached_threshold_counter = 0 # need this to be >1 to return true
+    # get the baseline for this trial
+    baseline = this_cell_trace[0:n_baseline_frames-1]
+    response = this_cell_trace[n_baseline_frames:]
 
-    # go through each frequency
-    for freq in cell_trace:
+    # get our threshold
+    peak_threshold = np.mean(baseline) + STD_threshold*np.std(baseline)
 
-        # go through each intensity it was played at:
-        for intensity in cell_trace[freq]:
+    # get the peak response
+    peak_response = np.amax(response)
 
-            # go through ecah repetition
-            for repetition in cell_trace[freq][intensity]:
-
-                this_trial = cell_trace[freq][intensity][repetition]
-
-                # get the baseline for this trial
-                baseline = this_trial[0:n_baseline_frames-1]
-                response = this_trial[n_baseline_frames:]
-
-                # get our threshold
-                peak_threshold = np.mean(baseline) + STD_threshold*np.std(baseline)
-
-                # get the peak response
-                peak_response = np.amax(response)
-
-                # if our peak response was above our threshold, increase the counter
-                if peak_response >= peak_threshold:
-                    reached_threshold_counter += 1
-
-    if reached_threshold_counter > 1:
+    # if our peak response was above our threshold, increase the counter
+    if peak_response >= peak_threshold:
         return True
     else:
         return False
@@ -69,7 +88,7 @@ def check_cell_zscore(cell_trace,zscore_threshold):
     avg_trace_zscores = zscore(avg_trace)
 
     # divide it into baseline and response
-    onset = round(EPOCH_START/1000 * fs * -1) # how many extra frames we have at the beginning before our stim onset
+    onset = round(EPOCH_START_IN_MS/1000 * FRAMERATE * -1) # how many extra frames we have at the beginning before our stim onset
     #baseline = avg_trace_zscores[:onset-1]
     response = avg_trace_zscores[onset:]
 
@@ -99,7 +118,7 @@ def get_avg_trace(cell_trace):
     summed_traces = np.zeros(shape=(n_trials,n_samples))
 
     counter = 0
-    # let's get a sum of all our traces
+    # let's get a sum of all our traces to average later
     for freq in cell_trace:
         for intensity in cell_trace[freq]:
             for repetition in cell_trace[freq][intensity]:
@@ -109,31 +128,31 @@ def get_avg_trace(cell_trace):
     return np.average(summed_traces,axis=0)
     
 
-def check_all_cells(traces):
+def check_all_cells(cell_dictionary,n_baseline_frames,threshold):
     
-    for cell in traces:
-        if (check_cell_zscore(traces[cell]['traces'],2)):
-            traces[cell]['active'] = True
+    for cell in cell_dictionary:
+        if (check_cell_STD(cell_dictionary[cell]['traces'],n_baseline_frames,threshold)):
+            cell_dictionary[cell]['active'] = True
         else:
-            traces[cell]['active'] = False
+            cell_dictionary[cell]['active'] = False
 
-    return traces
+    return cell_dictionary
 
 def main():
     
     # import our epoched and formatted recordings
     # again, it's formatted like this: 
-    # cell { freq { intensity { repetition: trace = [x,x,x,x,...] }}}
+    # cell { traces { freq { intensity { repetition: trace = [x,x,x,x,...] }}}}
     with open(BASE_PATH + traces_file, 'rb') as f:
-        traces = pickle.load(f)
+        cell_dictionary = pickle.load(f)
 
     # define some key variables we'll pass into our functions
-    late_peak = 1.5 - (EPOCH_START/1000) * fs # any peaks more than 1.5 seconds after onset will be considered late
-    n_baseline_frames = round(EPOCH_START/1000 * fs) # these are the frames we'll use as the baseline
-    STD_threshold = 3 # number of standard deviations from baseline
+    late_peak = 1.5 - (EPOCH_START_IN_MS/1000) * FRAMERATE # any peaks more than 1.5 seconds after onset will be considered late
+    n_baseline_frames = round(EPOCH_START_IN_MS/1000 * FRAMERATE) # these are the frames we'll use as the baseline
+    STD_threshold = 2 # number of standard deviations from baseline
 
 
-    traces_with_active_boolean = check_all_cells(traces)
+    traces_with_active_boolean = check_all_cells(cell_dictionary,n_baseline_frames,STD_threshold)
 
     # find the number of active cells
     counter = 0
@@ -145,10 +164,11 @@ def main():
     print("Number of active cells: ")
     print(counter)
 
+
     # print(len(get_avg_trace(traces[1])))
     # print(len(traces[1][1000][60][1]))
-    with open(BASE_PATH+output_file,'wb') as f:
-        pickle.dump(traces_with_active_boolean,f)
+    # with open(BASE_PATH+output_file,'wb') as f:
+    #     pickle.dump(traces_with_active_boolean,f)
 
 if __name__=='__main__':
     main()
--- src/plot_traces.py
+++
@@ -1,6 +0,0 @@
-import numpy as np 
-import matplotlib as plt
-
-movie = np.load("/Volumes/USB DISK/GCaMP6s_CL33/TSeries-11082021-1601-071-10Hz/suite2p/plane0/Fneu.npy")
-
-print(len(movie))
