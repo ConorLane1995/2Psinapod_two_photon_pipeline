870c04f3713a715002c0c6b1a12de8d668a22dbb
vtarka00 <vmtarka@gmail.com>
2022-02-23 12:26:39 -0500

    Added tuning by area under curve

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,13 @@ def get_cell_x(cell):
 def get_cell_y(cell):
     return cell["y"]
 
-def get_best_frequency(cell):
+def get_best_frequency_by_peak(cell):
     tuning_curve = cell['tuning_curve']
+    max_response_idx = np.argmax(tuning_curve[:,1])
+    return tuning_curve[max_response_idx,0]
+
+
+def get_best_frequency_by_area(cell):
+    tuning_curve = cell['tuning_curve_2']
     max_response_idx = np.argmax(tuning_curve[:,1])
     return tuning_curve[max_response_idx,0]
