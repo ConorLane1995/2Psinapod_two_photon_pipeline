b52027b78cbb574b0beaf11264a1e4847410f86f
Veronica Tarka <vmtarka@gmail.com>
2022-08-18 14:03:54 -0400

    Improved script to combine split recordings, doc improvements

--- twoP/visualization/tuning/plot_pre_post_tuning_traces.py
+++ twoP/visualization/tuning/plot_pre_post_tuning_traces.py
@@ -1,5 +1,8 @@
 """
-TODO  doc
+Plots a side by side of pre and post tuning for the cells specified in CELLS_TO_PLOT
+INPUT: cell dictionaries for the pre recording and post recording, recording_info.pkl for one of the two recordings (doesn't matter which)
+OUTPUT: (optional) saved figures for every cell listed in CELLS_TO_PLOT
+AUTHOR: Veronica Tarka, August 2022, veronica.tarka@mail.mcgill.ca
 """
 
 import matplotlib.gridspec as gridspec
@@ -7,8 +10,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 
+PRE_CELL_DICT_FILE = "/media/vtarka/USB DISK/Lab/2P/238_239_combined/Vid_238s/cells.pkl" # path to the cell dictionary for the pre-recording
+POST_CELL_DICT_FILE = "/media/vtarka/USB DISK/Lab/2P/238_239_combined/Vid_239s/cells.pkl" # path to the cell dictionary for the post-recording
+RECORDING_INFO_FILE = "/media/vtarka/USB DISK/Lab/2P/238_239_combined/Vid_239s/recording_info.pkl" # path to the recording info file, can be for just one of the recordings since they should use the same stim protocol
+FIGURE_FOLDER = "/media/vtarka/USB DISK/Lab/2P/238_239_combined/" # folder to save the figures in, set to None if you don't want them saved
+
+# set your list of cell IDs you'd like to plot
+CELLS_TO_PLOT = [2,5,10,27,31,39,48,52,81,103,123,133,150,240,257,1475]
+
 """
-TODO
+Get the largest value of the recording
+@param cell_traces: the contents of 'traces' for a single cell in the big dictionary
+@return the maximum value across all of the trial traces
 """
 def get_max_response(cell_traces):
 
@@ -22,10 +35,11 @@ def get_max_response(cell_traces):
                 
 """
 Plot the tuning traces for a single cell
-@param cell_traces_pre: the contents of the 'traces' key for a single cell in the big dictionary
-@param cell_traces_post: TODO
-@param n_frequencies: the total number of unique frequencies presented during the recording
-@param n_intensities: the total number of unique intensities presented during the recording
+@param cell_pre: the contents of a single cell key in the big dictionary (the pre-psilo recording)
+@param cell_post: the contents of the same single cell key in the big dictionary (the post-psilo recording)
+@param cell: the integer ID number of the cell (the key used to access the cell in the big dictionary)
+@param frequencies: a list of the unique frequencies presented during the recording (in ascending order)
+@param intensities: a list of the unique intensities presented during the recording (in ascending order)
 @param y_limit: how tall the y axis should be for each subplot
 """
 def plot_tuning_traces(cell_pre,cell_post,cell,frequencies,intensities,y_limit):
@@ -33,22 +47,26 @@ def plot_tuning_traces(cell_pre,cell_post,cell,frequencies,intensities,y_limit):
     f = plt.figure(figsize=(26,10))
     outer = gridspec.GridSpec(2,2,height_ratios=[4,1])
 
+    # get number of frequencies and intensities
     n_frequencies = len(frequencies)
     n_intensities = len(intensities)
 
     # PRE first
+    # add some labeling first
     ax = f.add_subplot(outer[0])
     if cell_pre['active']:
         plt.title("Pre (active)")
     else:
         plt.title("Pre (not active)")
     ax.axis('off')
+
+    # make our tuning curve grid
     inner = gridspec.GridSpecFromSubplotSpec(n_intensities,n_frequencies,subplot_spec=outer[0],wspace=0,hspace=0)
     cell_traces_pre = cell_pre['traces']
     for row,freq in zip(range(n_frequencies),cell_traces_pre.keys()):
         for col,itsy in zip(range(n_intensities),reversed(list(cell_traces_pre[freq].keys()))):
 
-            ax = plt.Subplot(f, inner[col,row])
+            ax = plt.Subplot(f, inner[col,row]) # make a subplot
 
             for rep in cell_traces_pre[freq][itsy]:
                 ax.plot(cell_traces_pre[freq][itsy][rep]) # plot every trial
@@ -67,12 +85,14 @@ def plot_tuning_traces(cell_pre,cell_post,cell,frequencies,intensities,y_limit):
             f.add_subplot(ax)
 
     # now POST
+    # add titles first
     ax = f.add_subplot(outer[1])
     if cell_post['active']:
         plt.title("Post (active)")
     else:
         plt.title("Post (not active)")
     ax.axis('off')
+
     inner = gridspec.GridSpecFromSubplotSpec(n_intensities,n_frequencies,subplot_spec=outer[1],wspace=0,hspace=0)
     cell_traces_post = cell_post['traces']
     for row,freq in zip(range(n_frequencies),cell_traces_post.keys()):
@@ -96,6 +116,8 @@ def plot_tuning_traces(cell_pre,cell_post,cell,frequencies,intensities,y_limit):
 
             f.add_subplot(ax)
 
+    # add the tuning heat maps
+    # PRE
     ax = f.add_subplot(outer[2])
     im = ax.imshow(np.transpose(cell_pre['tuning']),cmap='winter',origin='lower')
     ax.set_xticks(range(0,n_frequencies,2))
@@ -104,6 +126,7 @@ def plot_tuning_traces(cell_pre,cell_post,cell,frequencies,intensities,y_limit):
     ax.set_yticklabels(intensities)
     plt.colorbar(im,ax=ax,format=lambda x, _:f"{x:4.0f}")
 
+    # POST
     ax = f.add_subplot(outer[3])
     im = ax.imshow(np.transpose(cell_post['tuning']),cmap='winter',origin='lower')
     ax.set_xticks(range(0,n_frequencies,2))
@@ -115,28 +138,27 @@ def plot_tuning_traces(cell_pre,cell_post,cell,frequencies,intensities,y_limit):
     f.text(0.5,0.01,"Frequency (Hz)",va='center',ha='center')
     f.text(0.01,0.5,"Intensity (dB)",va='center',ha='center',rotation='vertical')
     f.suptitle(cell)
-    plt.savefig("/media/vtarka/USB DISK/Lab/2P/238_239_combined/"+"pre_post_{}.png".format(cell))
+    if FIGURE_FOLDER != None:
+        plt.savefig(FIGURE_FOLDER+"pre_post_{}.png".format(cell))
     plt.show(block=False)
 
 
 def main():
 
-    with open("/media/vtarka/USB DISK/Lab/2P/238_239_combined/Vid_238s/cells.pkl","rb") as f:
+    with open(PRE_CELL_DICT_FILE,"rb") as f:
         r1_cells = pickle.load(f)
 
-    with open("/media/vtarka/USB DISK/Lab/2P/238_239_combined/Vid_239s/cells.pkl","rb") as f:
+    with open(POST_CELL_DICT_FILE,"rb") as f:
         r2_cells = pickle.load(f)
 
     # load the recording info file
-    with open("/media/vtarka/USB DISK/Lab/2P/238_239_combined/Vid_239s/" + "recording_info.pkl","rb") as f:
+    with open(RECORDING_INFO_FILE,"rb") as f:
         recording_info = pickle.load(f)
 
     frequencies = recording_info['frequencies']
     intensities = recording_info['intensities']
 
-    cells_to_plot = [2,5,10,27,31,39,48,52,81,103,123,133,150,240,257,1475]
-
-    for cell in cells_to_plot:
+    for cell in CELLS_TO_PLOT:
         highest_point = max([get_max_response(r1_cells[cell]['traces']),get_max_response(r2_cells[cell]['traces'])])
         plot_tuning_traces(r1_cells[cell],r2_cells[cell],cell,frequencies,intensities,highest_point+(highest_point*0.1))
     
